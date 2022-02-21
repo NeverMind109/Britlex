cfb85a4d396cfd37c429152d7dc9aeeefe0f2302
NeverMind109 <vladgladkih123@gmail.com>
2022-02-22 01:55:04 +0300

    Created: Added Header with adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
 $(function () {
-    
-})
+  $(".menu__mobile").on("click", function () {
+    $(".menu").toggleClass("menu--active");
+    $("body").toggleClass("overflow-hidden");
+    $(".menu-list__link").on("click", function () {
+      $(".menu").removeClass("menu--active");
+      $("body").removeClass("overflow-hidden");
+    });
+    $(".logo").on("click", function () {
+      $(".menu").removeClass("menu--active");
+      $("body").removeClass("overflow-hidden");
+    });
+  });
+});
