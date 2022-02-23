f9ebe588cd31344664f41fd7783bcc0442ed80d2
NeverMind109 <vladgladkih123@gmail.com>
2022-02-23 03:57:53 +0300

    Created: Added Mobile Menu

--- app/js/main.js
+++ app/js/main.js
@@ -2,13 +2,16 @@ $(function () {
   $(".menu__mobile").on("click", function () {
     $(".menu").toggleClass("menu--active");
     $("body").toggleClass("overflow-hidden");
-    $(".menu-list__link").on("click", function () {
-      $(".menu").removeClass("menu--active");
-      $("body").removeClass("overflow-hidden");
-    });
-    $(".logo").on("click", function () {
-      $(".menu").removeClass("menu--active");
-      $("body").removeClass("overflow-hidden");
-    });
+    $(".menu__mobile-layout").toggleClass("menu__mobile-layout--active");
   });
+
+  $(".menu__logo").on("click", mobileMenuClose);
+  $(".menu__close-button").on("click", mobileMenuClose);
+  $(".menu__mobile-layout").on("click", mobileMenuClose);
+  $(".menu-list__link").on("click", mobileMenuClose);
+  function mobileMenuClose() {
+    $(".menu").removeClass("menu--active");
+    $("body").removeClass("overflow-hidden");
+    $(".menu__mobile-layout").removeClass("menu__mobile-layout--active");
+  }
 });
